2cf0761fda81899a3ff23db5b90a74645044c45c
1Anand1 <anand22dandavate@gmail.com>
2025-07-24 21:48:44 +0530

    Solved the two sum problem.

--- Easy/1.two-sum.py
+++ Easy/1.two-sum.py
@@ -4,9 +4,26 @@
 # [1] Two Sum
 #
 
+from typing import List
+
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # Started a dictionary similar to hash map
+        myDict={}
+
+        # Going through eachNum
+        for index , eachNum in enumerate(nums):
+            
+            # Calculating the reminder
+            rem = target - eachNum
+
+            if rem in myDict.keys():
+                return [myDict[rem] , index]
+
+            myDict[eachNum] = index
+
         
 # @lc code=end
 
