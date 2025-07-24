daafb7b9f3042511b89c6cc8e135490c2122d6f6
1Anand1 <anand22dandavate@gmail.com>
2025-07-24 21:55:59 +0530

    Started Palindrome number problem.

---
+++ Easy/9.palindrome-number.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+# Follow up: Could you solve it without converting the integer to a string?
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+# @lc code=end
+
